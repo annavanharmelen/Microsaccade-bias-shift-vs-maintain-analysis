59f8c584ce862229cf566fc8b514cea736405b04
Anna <anna@vanharmelen.me>
2025-06-11 14:50:46 +0200

    fix zlim of sizecourse

--- GA_SaccadeBias.m
+++ GA_SaccadeBias.m
@@ -350,7 +350,7 @@ if plotGAs
     cfg = [];
     cfg.parameter = 'avg_data';
     cfg.figure = 'gcf';
-    cfg.zlim = 'maxabs';
+    cfg.zlim = [-0.01, 0.01];
     cfg.xlim = xlimtoplot;  
     cfg.colormap = brewermap(1000, 'PRGn');
     
