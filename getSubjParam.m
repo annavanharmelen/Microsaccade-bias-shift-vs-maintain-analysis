c8f06c82957c82a7ebe7542a288242aeff9b317b
Anna <anna@vanharmelen.me>
2026-04-10 14:46:12 +0200

    update datadir

--- getSubjParam.m
+++ getSubjParam.m
@@ -5,7 +5,7 @@ function param = getSubjParam(pp)
 %% set path and pp-specific file locations
 unique_numbers = [65, 34, 56, 26, 97, 15, 78, 70, 44, 67, 61, 17, 86, 10, 28, 48, 12, 58, 45, 14, 11, 40, 96, 55, 85, 47, 71, 25, 13]; %needs to be in the right order
 
-param.path = '\\labsdfs.labs.vu.nl\labsdfs\FGB-ETP-CogPsy-ProactiveBrainLab\core_lab_members\Anna\Data\m2 - microsaccade bias shift vs. sustain with fixational control\';
+param.path = '\\scistor.vu.nl\shares\FGB-ETP-CogPsy-ProactiveBrainLab\core_lab_members\Anna\Data\m2 - microsaccade bias shift vs. sustain with fixational control\';
 
 if pp < 10
     param.subjName = sprintf('pp0%d', pp);
