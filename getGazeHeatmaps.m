1dd0fcd08f5330dddf3bf1bc8ca7062898e976c0
Anna <anna@vanharmelen.me>
2026-05-08 11:58:26 +0200

    create gaze heatmap figure

--- getGazeHeatmaps.m
+++ getGazeHeatmaps.m
@@ -79,7 +79,7 @@ for pp = pp2do
 
 end
 
-%% Make supplementary gaze heatplot figure
+%% Creat data for supplementary gaze heatplot figure
 bins = [-6:0.05:6];
 window = 0.1;
 shift = 701:1100;
@@ -98,169 +98,249 @@ maintain_L = histcounts2(L_x_data(:,:,maintain), L_y_data(:,:,maintain), edges, 
 maintain_R = histcounts2(R_x_data(:,:,maintain), R_y_data(:,:,maintain), edges, edges, 'Normalization', 'probability');
 maintain_diff = maintain_L - maintain_R;
 
-%% Plot
-c_lim = [-0.005, 0.005];
-c_lim_diff = [-0.0005, 0.0005];
+if plotResults
+    %% Plot all time periods
+    c_lim = [-0.005, 0.005];
+    c_lim_diff = [-0.0005, 0.0005];
+    
+    figure;
+    % whole trial period - L
+    subplot(3,3,1)
+    hold on
+    imagesc('XData', bins, 'YData', bins, 'CData', whole_L');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
+    colorbar
+    ax = gca();
+    ax.YTick = [-6:3:6];
+    ax.XTick = [-6:3:6];
+    ax.TickLength = [0.015 0.03];
+    xlim([-6,6])
+    ylim([-6,6])
+    set(gca, 'FontSize', 20,'Fontname','Aptos', 'LineWidth', 1.5);
+    set(gca,'TickDir','out');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim)
+    title('L')
+    
+    % whole trial period - R
+    subplot(3,3,2)
+    hold on
+    imagesc('XData', bins, 'YData', bins, 'CData', whole_R');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
+    colorbar
+    ax = gca();
+    ax.YTick = [-6:3:6];
+    ax.XTick = [-6:3:6];
+    ax.TickLength = [0.015 0.03];
+    xlim([-6,6])
+    ylim([-6,6])
+    set(gca, 'FontSize', 20,'Fontname','Aptos', 'LineWidth', 1.5);
+    set(gca,'TickDir','out');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim)
+    title('R')
+    
+    % whole trial period - diff
+    subplot(3,3,3)
+    hold on
+    imagesc('XData', bins, 'YData', bins, 'CData', whole_diff');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
+    colorbar
+    ax = gca();
+    ax.YTick = [-6:3:6];
+    ax.XTick = [-6:3:6];
+    ax.TickLength = [0.015 0.03];
+    xlim([-6,6])
+    ylim([-6,6])
+    set(gca, 'FontSize', 20,'Fontname','Aptos', 'LineWidth', 1.5);
+    set(gca,'TickDir','out');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim_diff)
+    title('diff')
+    
+    % shift trial period - L
+    subplot(3,3,4)
+    hold on
+    imagesc('XData', bins, 'YData', bins, 'CData', shift_L');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
+    colorbar
+    ax = gca();
+    ax.YTick = [-6:3:6];
+    ax.XTick = [-6:3:6];
+    ax.TickLength = [0.015 0.03];
+    xlim([-6,6])
+    ylim([-6,6])
+    set(gca, 'FontSize', 20,'Fontname','Aptos', 'LineWidth', 1.5);
+    set(gca,'TickDir','out');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim)
+    title('L')
+    
+    % shift trial period - R
+    subplot(3,3,5)
+    hold on
+    imagesc('XData', bins, 'YData', bins, 'CData', shift_R');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
+    colorbar
+    ax = gca();
+    ax.YTick = [-6:3:6];
+    ax.XTick = [-6:3:6];
+    ax.TickLength = [0.015 0.03];
+    xlim([-6,6])
+    ylim([-6,6])
+    set(gca, 'FontSize', 20,'Fontname','Aptos', 'LineWidth', 1.5);
+    set(gca,'TickDir','out');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim)
+    title('R')
+    
+    % shift trial period - diff
+    subplot(3,3,6)
+    hold on
+    imagesc('XData', bins, 'YData', bins, 'CData', shift_diff');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
+    colorbar
+    ax = gca();
+    ax.YTick = [-6:3:6];
+    ax.XTick = [-6:3:6];
+    ax.TickLength = [0.015 0.03];
+    xlim([-6,6])
+    ylim([-6,6])
+    set(gca, 'FontSize', 20,'Fontname','Aptos', 'LineWidth', 1.5);
+    set(gca,'TickDir','out');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim_diff)
+    title('diff')
+    
+    % maintain trial period - L
+    subplot(3,3,7)
+    hold on
+    imagesc('XData', bins, 'YData', bins, 'CData', maintain_L');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
+    colorbar
+    ax = gca();
+    ax.YTick = [-6:3:6];
+    ax.XTick = [-6:3:6];
+    ax.TickLength = [0.015 0.03];
+    xlim([-6,6])
+    ylim([-6,6])
+    set(gca, 'FontSize', 20,'Fontname','Aptos', 'LineWidth', 1.5);
+    set(gca,'TickDir','out');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim)
+    title('L')
+    
+    % maintain trial period - R
+    subplot(3,3,8)
+    hold on
+    imagesc('XData', bins, 'YData', bins, 'CData', maintain_R');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
+    colorbar
+    ax = gca();
+    ax.YTick = [-6:3:6];
+    ax.XTick = [-6:3:6];
+    ax.TickLength = [0.015 0.03];
+    xlim([-6,6])
+    ylim([-6,6])
+    set(gca, 'FontSize', 20,'Fontname','Aptos', 'LineWidth', 1.5);
+    set(gca,'TickDir','out');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim)
+    title('R')
+    
+    % maintain trial period - diff
+    subplot(3,3,9)
+    hold on
+    imagesc('XData', bins, 'YData', bins, 'CData', maintain_diff');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
+    colorbar
+    ax = gca();
+    ax.YTick = [-6:3:6];
+    ax.XTick = [-6:3:6];
+    ax.TickLength = [0.015 0.03];
+    xlim([-6,6])
+    ylim([-6,6])
+    set(gca, 'FontSize', 20,'Fontname','Aptos', 'LineWidth', 1.5);
+    set(gca,'TickDir','out');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim_diff)
+    title('diff')
 
-figure;
-% whole trial period - L
-subplot(3,3,1)
-hold on
-imagesc('XData', bins, 'YData', bins, 'CData', whole_L');
-plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
-colorbar
-ax = gca();
-ax.YTick = [-6:3:6];
-ax.XTick = [-6:3:6];
-ax.TickLength = [0.015 0.03];
-xlim([-6,6])
-ylim([-6,6])
-set(gca, 'FontSize', 20,'Fontname','Arial', 'LineWidth', 1.5);
-set(gca,'TickDir','out');
-colormap(brewermap(1000, 'PRGn'));
-caxis(c_lim)
-title('L')
+    %% Plot shift period only for supplementary figure
+    c_lim = [-0.006, 0.006];
+    c_lim_diff = [-0.0006, 0.0006];
+    
+    figure;
+    % shift trial period - L
+    L = subplot(1,3,1);
+    hold on
+    imagesc('XData', bins(6:236), 'YData', bins(6:236), 'CData', shift_L(6:236,6:236)');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi), '--', 'LineWidth', 2.6, 'Color', [0.6, 0.6, 0.6]);
+    yticks([-6:3:6]);
+    xticks([-6:3:6]);
+    xlim([-6,6]);
+    ylim([-6,6]);
+    ylabel('Y-position gaze ( )');
+    colormap(brewermap(1000, 'PRGn'));
+    colorbar('off');
+    caxis(c_lim)
+    title('Left')
+    
+    % shift trial period - R
+    M = subplot(1,3,2);
+    hold on
+    imagesc('XData', bins(6:236), 'YData', bins(6:236), 'CData', shift_R(6:236,6:236)');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi), '--', 'LineWidth', 2.6, 'Color', [0.6, 0.6, 0.6]);
+    yticks([-6:3:6]);
+    xticks([-6:3:6]);
+    xlim([-6,6]);
+    ylim([-6,6]);
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim)
+    c1 = colorbar();
+    c1.Position = [0.5307 0.37 0.012 0.30];
+    c1.Ticks = [-0.005, 0, 0.005];
+    c1.TickLabels = {[]};
+    title('Right')
+    
+    % shift trial period - diff
+    R = subplot(1,3,3);
+    hold on
+    imagesc('XData', bins(6:236), 'YData', bins(6:236), 'CData', shift_diff(6:236,6:236)');
+    plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi), '--', 'LineWidth', 2.6, 'Color', [0.6, 0.6, 0.6]);
+    yticks([-6:3:6]);
+    xticks([-6:3:6]);
+    xlim([-6,6]);
+    ylim([-6,6]);
+    ylabel('Y-position gaze ( )');
+    colormap(brewermap(1000, 'PRGn'));
+    caxis(c_lim_diff)
+    c2 = colorbar();
+    c2.Position = [0.8729 0.37 0.012 0.30];
+    c2.Ticks=[-0.005, 0, 0.005];
+    c2.TickLabels = {[]};
+    title('Left vs. Right')
+    
+    % general
+    set(gcf(), 'Position', [500 500 1500 400]);
 
-% whole trial period - R
-subplot(3,3,2)
-hold on
-imagesc('XData', bins, 'YData', bins, 'CData', whole_R');
-plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
-colorbar
-ax = gca();
-ax.YTick = [-6:3:6];
-ax.XTick = [-6:3:6];
-ax.TickLength = [0.015 0.03];
-xlim([-6,6])
-ylim([-6,6])
-set(gca, 'FontSize', 20,'Fontname','Arial', 'LineWidth', 1.5);
-set(gca,'TickDir','out');
-colormap(brewermap(1000, 'PRGn'));
-caxis(c_lim)
-title('R')
+    axes = {L, M, R};
+    for i = 1:size(axes,2)
+        xlabel(axes{i}, 'X-position gaze ( )');
+        set(axes{i}, 'Box', 'on');
+        set(axes{i}, 'TickLabelInterpreter', 'tex');
+        set(axes{i}, 'FontSize', [20.7]);
+        set(axes{i}, 'FontName', 'Aptos');
+        set(axes{i}, 'LineWidth', 1);
+        axis(axes{i}, "square");
+    end
 
-% whole trial period - diff
-subplot(3,3,3)
-hold on
-imagesc('XData', bins, 'YData', bins, 'CData', whole_diff');
-plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
-colorbar
-ax = gca();
-ax.YTick = [-6:3:6];
-ax.XTick = [-6:3:6];
-ax.TickLength = [0.015 0.03];
-xlim([-6,6])
-ylim([-6,6])
-set(gca, 'FontSize', 20,'Fontname','Arial', 'LineWidth', 1.5);
-set(gca,'TickDir','out');
-colormap(brewermap(1000, 'PRGn'));
-caxis(c_lim_diff)
-title('diff')
+    set(L, 'position', [0.1300 0.1100 0.1703 0.8150]);
+    set(M, 'position', [0.3500 0.1100 0.1703 0.8150]);
+    set(R, 'position', [0.6916 0.1100 0.1703 0.8150]);
 
-% shift trial period - L
-subplot(3,3,4)
-hold on
-imagesc('XData', bins, 'YData', bins, 'CData', shift_L');
-plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
-colorbar
-ax = gca();
-ax.YTick = [-6:3:6];
-ax.XTick = [-6:3:6];
-ax.TickLength = [0.015 0.03];
-xlim([-6,6])
-ylim([-6,6])
-set(gca, 'FontSize', 20,'Fontname','Arial', 'LineWidth', 1.5);
-set(gca,'TickDir','out');
-colormap(brewermap(1000, 'PRGn'));
-caxis(c_lim)
-title('L')
+    set(gcf, 'Renderer', 'Painters');
 
-% shift trial period - R
-subplot(3,3,5)
-hold on
-imagesc('XData', bins, 'YData', bins, 'CData', shift_R');
-plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
-colorbar
-ax = gca();
-ax.YTick = [-6:3:6];
-ax.XTick = [-6:3:6];
-ax.TickLength = [0.015 0.03];
-xlim([-6,6])
-ylim([-6,6])
-set(gca, 'FontSize', 20,'Fontname','Arial', 'LineWidth', 1.5);
-set(gca,'TickDir','out');
-colormap(brewermap(1000, 'PRGn'));
-caxis(c_lim)
-title('R')
-
-% shift trial period - diff
-subplot(3,3,6)
-hold on
-imagesc('XData', bins, 'YData', bins, 'CData', shift_diff');
-plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
-colorbar
-ax = gca();
-ax.YTick = [-6:3:6];
-ax.XTick = [-6:3:6];
-ax.TickLength = [0.015 0.03];
-xlim([-6,6])
-ylim([-6,6])
-set(gca, 'FontSize', 20,'Fontname','Arial', 'LineWidth', 1.5);
-set(gca,'TickDir','out');
-colormap(brewermap(1000, 'PRGn'));
-caxis(c_lim_diff)
-title('diff')
-
-% maintain trial period - L
-subplot(3,3,7)
-hold on
-imagesc('XData', bins, 'YData', bins, 'CData', maintain_L');
-plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
-colorbar
-ax = gca();
-ax.YTick = [-6:3:6];
-ax.XTick = [-6:3:6];
-ax.TickLength = [0.015 0.03];
-xlim([-6,6])
-ylim([-6,6])
-set(gca, 'FontSize', 20,'Fontname','Arial', 'LineWidth', 1.5);
-set(gca,'TickDir','out');
-colormap(brewermap(1000, 'PRGn'));
-caxis(c_lim)
-title('L')
-
-% maintain trial period - R
-subplot(3,3,8)
-hold on
-imagesc('XData', bins, 'YData', bins, 'CData', maintain_R');
-plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
-colorbar
-ax = gca();
-ax.YTick = [-6:3:6];
-ax.XTick = [-6:3:6];
-ax.TickLength = [0.015 0.03];
-xlim([-6,6])
-ylim([-6,6])
-set(gca, 'FontSize', 20,'Fontname','Arial', 'LineWidth', 1.5);
-set(gca,'TickDir','out');
-colormap(brewermap(1000, 'PRGn'));
-caxis(c_lim)
-title('R')
-
-% maintain trial period - diff
-subplot(3,3,9)
-hold on
-imagesc('XData', bins, 'YData', bins, 'CData', maintain_diff');
-plot(2 * cos(0:pi/50:2*pi), 2 * sin(0:pi/50:2*pi));
-colorbar
-ax = gca();
-ax.YTick = [-6:3:6];
-ax.XTick = [-6:3:6];
-ax.TickLength = [0.015 0.03];
-xlim([-6,6])
-ylim([-6,6])
-set(gca, 'FontSize', 20,'Fontname','Arial', 'LineWidth', 1.5);
-set(gca,'TickDir','out');
-colormap(brewermap(1000, 'PRGn'));
-caxis(c_lim_diff)
-title('diff')
+    print("..\..\..\..\Manuscripts\Shift-vs.-maintain\Figures\supl_gazeheatmaps_E1", "-dsvg", "-vector")
+    print("..\..\..\..\Manuscripts\Shift-vs.-maintain\Figures\supl_gazeheatmaps_E1", "-dpng")
+end
