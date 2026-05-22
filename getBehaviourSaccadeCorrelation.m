19f997c7122869d9c9d43330d40f71818403c6f0
Anna <anna@vanharmelen.me>
2026-05-22 11:16:03 +0200

    update figure for behavioural x saccade bias correlation

--- getBehaviourSaccadeCorrelation.m
+++ getBehaviourSaccadeCorrelation.m
@@ -2,24 +2,53 @@
 % Run both getBehaviour.m and GA_SaccadeBias.m first
 
 rt_effect = reaction_time_validity(:,1) - reaction_time_validity(:,2);
-acc_effect = error_validity(:,1) - error_validity(:,2);
+acc_effect = (error_validity(:,1) - error_validity(:,2))*100;
 
-[rt_r,rt_p] = corr(rt_effect, avg_saccade_effect(:,1), 'Type', 'Pearson');
-[acc_r,acc_p] = corr(acc_effect, avg_saccade_effect(:,1), 'Type', 'Pearson');
+[rt_r_p,rt_p_p] = corr(rt_effect, avg_saccade_effect(:,1), 'Type', 'Pearson')
+[rt_r_s,rt_p_s] = corr(rt_effect, avg_saccade_effect(:,1), 'Type', 'Spearman')
+[acc_r_p,acc_p_p] = corr(acc_effect, avg_saccade_effect(:,1), 'Type', 'Pearson')
+[acc_r_s,acc_p_s] = corr(acc_effect, avg_saccade_effect(:,1), 'Type', 'Spearman')
 
 figure;
-subplot(1,2,1)
-scatter(-rt_effect, avg_saccade_effect(:,1), 'k', 'filled');
-lsline;
-xline(0, '--');
-yline(0, '--');
-ylabel('Avg. saccade bias 200-600 ms (ΔHz)');
+rt = subplot(2,1,1);
+hold on
+plot([-200,800], [0,0], '--', 'LineWidth',2, 'Color', [0.6, 0.6, 0.6]);
+plot([0,0], [-0.05, 0.05], '--', 'LineWidth',2, 'Color', [0.6, 0.6, 0.6]);
+scatter(-rt_effect, avg_saccade_effect(:,1), 60, 'k', 'filled');
+line1 = lsline;
+line1.LineWidth = 2;
+line1.Color = [0,0,0];
+ylim([-0.05, 0.05]);
+xlim([-200, 800]);
+xticks([0, 400, 800]);
+ylabel('Saccade bias (ΔHz)');
 xlabel('RT benefit (ms)');
+axis('square');
 
-subplot(1,2,2)
-scatter(acc_effect, avg_saccade_effect(:,1), 'k', 'filled');
-lsline;
-xline(0, '--');
-yline(0, '--');
-ylabel('Avg. saccade bias 200-600 ms (ΔHz)');
-xlabel('Accuracy benefit (ms)');
+acc = subplot(2,1,2);
+hold on
+plot([-20,80], [0,0], '--', 'LineWidth',2, 'Color', [0.6, 0.6, 0.6]);
+plot([0,0], [-0.05, 0.05], '--', 'LineWidth',2, 'Color', [0.6, 0.6, 0.6]);
+scatter(acc_effect, avg_saccade_effect(:,1), 60, 'k', 'filled');
+line2 = lsline;
+line2.LineWidth = 2;
+line2.Color = [0,0,0];
+ylim([-0.05, 0.05]);
+xlim([-20, 80]);
+xticks([0, 40, 80]);
+ylabel('Saccade bias (ΔHz)');
+xlabel('Accuracy benefit (%)');
+axis('square');
+
+set(gcf(), 'Position', [600 250 680 1060]);
+
+axes = {rt, acc};
+for i = 1:size(axes,2)
+    set(axes{i}, 'Box', 'on');
+    set(axes{i}, 'FontSize', [24.6]);
+    set(axes{i}, 'FontName', 'Aptos');
+    set(axes{i}, 'LineWidth', 1);
+end
+
+print("..\..\..\..\Manuscripts\Shift-vs.-maintain\Figures\supl_behaviour_x_bias_E2", "-dsvg")
+print("..\..\..\..\Manuscripts\Shift-vs.-maintain\Figures\supl_behaviour_x_bias_E2", "-dpng")
