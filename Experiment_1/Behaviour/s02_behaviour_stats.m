dec5f5ae743039f66290c6450f7a022fe9a8dc02
Anna <anna@vanharmelen.me>
2026-08-25 10:29:15 +0200

    fix name of confidence interval

--- Experiment_1/Behaviour/s02_behaviour_stats.m
+++ Experiment_1/Behaviour/s02_behaviour_stats.m
@@ -1,6 +1,6 @@
 %% Bar stats
-[h,p,i,stats] = ttest(reaction_time_validity(:,1), reaction_time_validity(:,2))
-[h,p,i,stats] = ttest(error_validity(:,1), error_validity(:,2))
+[h,p,ci,stats] = ttest(reaction_time_validity(:,1), reaction_time_validity(:,2))
+[h,p,ci,stats] = ttest(error_validity(:,1), error_validity(:,2))
 
 % Cohens d's for paired samples
 rt_d = meanEffectSize(reaction_time_validity(:,1), reaction_time_validity(:,2), "Paired", true, "Effect", "cohen") 
@@ -9,10 +9,10 @@ acc_d = meanEffectSize(error_validity(:,1), error_validity(:,2), "Paired", true,
 %% SOA t-test stats
 for soa = 1:size(trial_lengths, 2)
     disp(['soa: ', num2str(trial_lengths(soa))])
-    [h,p,i,stats] = ttest(reaction_time_per_soa_valid(:,soa), reaction_time_per_soa_invalid(:,soa));
+    [h,p,ci,stats] = ttest(reaction_time_per_soa_valid(:,soa), reaction_time_per_soa_invalid(:,soa));
     disp(['rt: ', 'p=', num2str(p)])
     disp(stats)
-    [h,p,i,stats] = ttest(accuracy_per_soa_valid(:,soa), accuracy_per_soa_invalid(:,soa));
+    [h,p,ci,stats] = ttest(accuracy_per_soa_valid(:,soa), accuracy_per_soa_invalid(:,soa));
     disp(['acc: ', 'p=', num2str(p)])
     disp(stats)
     disp(' ');
